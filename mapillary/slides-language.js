df45f41a0068f6fe340c8f0f83a92c2965581bec
edilqueirozdearaujo <edileqa@gmail.com>
2016-06-09 11:26:10 -0300

    antes-depois

--- mapillary/slides-language.js
+++ mapillary/slides-language.js
@@ -7,6 +7,8 @@ if ( userLang != 'pt-BR' && userLang != 'pt' ) {
 	$('.txtBtnMP1').text('Mapillary Photo 1');
 	$('.txtBtnMP2').text('Mapillary Photo 2');
 	$('.ImgHeader').text('Time travel with Mapillary Streetview');
+	$('.txtInfoComment').text('Visit these photos at Mapillary website to read and post comments, and learn more about the place, clicking int the buttons below:');
+	$('.txtStreetview').text('This place into Streetview');
 	
 
 
